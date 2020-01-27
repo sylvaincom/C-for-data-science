283e468ecca9834c93a0a01bd94104a66ee4e569
JulienMLR <54364388+JulienMLR@users.noreply.github.com>
2020-01-27 02:16:59 +0100

    Update main.cpp

--- kNN/main.cpp
+++ kNN/main.cpp
@@ -1,25 +1,5 @@
 #include <iostream>
-//#include "Kmeans.h"
 #include "KNN.h"
-
-/*int main() {
-
-    std::ostream &output_console = std::cout;
-
-    //Pour un fichier choisi
-    std::ifstream input;
-    input.open("C:/Users/julie/Desktop/test_points.txt");   //à adapter en fonction du répertoire
-    kmeans_set km(input, ',');
-
-    //Pour une génération de points aléatoire
-    kmeans_set kmeans_set(20);
-
-    kmeans_set.algo_kmeans(4);
-    std::cout << "\n\nOutput in main():\n";
-    kmeans_set.print_centroids(output_console, ',');
-    return 0;
-
-}*/
 
 int main(){
     std::ostream &output_console = std::cout;
